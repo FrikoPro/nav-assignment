253c0b30b05575af7b8207f12bac9348bdad64c3
FrikoPro <jansenfredrik@gmail.com>
2021-02-04 10:51:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { useState } from 'react';
+import { Button, Card, Container, Form, Row } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const g = 101351;
+
+	const [input, setInput] = useState({
+		input1: '',
+		input2: '',
+		input3: '',
+	});
+
+	const [result, setResult] = useState('');
+
+	const [textHelp, setTextHelp] = useState({
+		textHelp1: '',
+		textHelp2: '',
+		textHelp3: '',
+	});
+
+	const checkValidSalary = () => {
+		const lastYear = parseInt(input.input1);
+		const secondYear = parseInt(input.input2);
+		const thirdYear = parseInt(input.input3);
+
+		const totalInntekt = lastYear + secondYear + thirdYear;
+
+		if (lastYear <= 0) {
+			return setResult(<h5>Du er ikke innvilget for dagpenger</h5>);
+		} else if (totalInntekt <= 3 * g || thirdYear <= 1.5 * g) {
+			return setResult(<h5>Du er ikke innvilget for dagpenger</h5>);
+		} else if (
+			lastYear <= 6 * g ||
+			(thirdYear + lastYear + secondYear) / 3 <= 6 * g
+		) {
+			const dagsats = lastYear / 260;
+
+			return setResult(
+				<div>
+					<h5>Du har rett til dagpenger</h5>
+					<h5>din dagsats: {Math.floor(dagsats)}</h5>
+				</div>
+			);
+		} else {
+			return setResult(<h5>Du er ikke innvilget for dagpenger</h5>);
+		}
+	};
+
+	const checkInput = () => {
+		var numbers = /^[0-9]+$/;
+
+		setResult('');
+
+		const isInputValid = (val) => {
+			if (val === '') return 'Du må fylle inn feltet';
+			else if (!val.match(numbers)) return 'Du kan bare taste inn tall (0-9)';
+			else return '';
+		};
+
+		let newState = {};
+
+		for (var [key, val] of Object.entries(input)) {
+			switch (key) {
+				case 'input1':
+					newState = { ...newState, textHelp1: isInputValid(val) };
+					break;
+				case 'input2':
+					newState = { ...newState, textHelp2: isInputValid(val) };
+					break;
+				case 'input3':
+					newState = { ...newState, textHelp3: isInputValid(val) };
+					break;
+				default:
+					return;
+			}
+		}
+
+		setTextHelp(newState);
+
+		if (
+			newState.textHelp1 === '' &&
+			newState.textHelp2 === '' &&
+			newState.textHelp3 === ''
+		) {
+			console.log('valid');
+			checkValidSalary();
+		}
+	};
+
+	return (
+		<Container className="h-100">
+			<Row className="h-100">
+				<Card className="w-75 mx-auto my-auto">
+					<Card.Body className="pl-5">
+						<Card.Title>Sjekke grunnlag for dagpenger</Card.Title>
+						<Form>
+							<Form.Group controlId="formSalary1">
+								<Form.Label>Hva tjente du i fjor?</Form.Label>
+								<Form.Control
+									type="text"
+									onChange={(e) =>
+										setInput({ ...input, input1: e.target.value })
+									}
+								/>
+								<Form.Text className="text-danger">
+									{textHelp.textHelp1}
+								</Form.Text>
+							</Form.Group>
+							<Form.Group controlId="formSalary2">
+								<Form.Label>Hva tjente du for 2 år siden?</Form.Label>
+								<Form.Control
+									onChange={(e) =>
+										setInput({ ...input, input2: e.target.value })
+									}
+								/>
+								<Form.Text className="text-danger">
+									{textHelp.textHelp2}
+								</Form.Text>
+							</Form.Group>
+							<Form.Group controlId="formSalary3">
+								<Form.Label>Hva tjente du for 3 år siden?</Form.Label>
+								<Form.Control
+									onChange={(e) =>
+										setInput({ ...input, input3: e.target.value })
+									}
+								/>
+								<Form.Text className="text-danger">
+									{textHelp.textHelp3}
+								</Form.Text>
+							</Form.Group>
+							<Button onClick={checkInput}>Sjekk</Button>
+						</Form>
+						<Row className="justify-content-center text-center">{result}</Row>
+					</Card.Body>
+				</Card>
+			</Row>
+		</Container>
+	);
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Test user interaction when response is valid', () => {
+	const { getByLabelText, getByText } = render(<App />);
+
+	const input1 = getByLabelText('Hva tjente du i fjor?');
+	const input2 = getByLabelText('Hva tjente du for 2 år siden?');
+	const input3 = getByLabelText('Hva tjente du for 3 år siden?');
+
+	fireEvent.change(input1, { target: { value: '500000' } });
+	fireEvent.change(input2, { target: { value: '450000' } });
+	fireEvent.change(input3, { target: { value: '400000' } });
+	fireEvent.click(getByText('Sjekk'));
+
+	getByText('Du har rett til dagpenger');
+	getByText('din dagsats: 1923');
+});
+
+test('test user interaction when response is not valid', () => {
+	const { getByLabelText, getByText } = render(<App />);
+
+	const input1 = getByLabelText('Hva tjente du i fjor?');
+	const input2 = getByLabelText('Hva tjente du for 2 år siden?');
+	const input3 = getByLabelText('Hva tjente du for 3 år siden?');
+
+	fireEvent.change(input1, { target: { value: '1000000' } });
+	fireEvent.change(input2, { target: { value: '450000' } });
+	fireEvent.change(input3, { target: { value: '400000' } });
+	fireEvent.click(getByText('Sjekk'));
+
+	getByText('Du er ikke innvilget for dagpenger');
+});
+
+test('test user interaction when typing wrong value', () => {
+	const { getByLabelText, getByText } = render(<App />);
+
+	const input1 = getByLabelText('Hva tjente du i fjor?');
+	const input2 = getByLabelText('Hva tjente du for 2 år siden?');
+	const input3 = getByLabelText('Hva tjente du for 3 år siden?');
+
+	fireEvent.change(input1, { target: { value: 'f' } });
+	fireEvent.change(input2, { target: { value: '' } });
+	fireEvent.change(input3, { target: { value: '1' } });
+	fireEvent.click(getByText('Sjekk'));
+
+	getByText('Du må fylle inn feltet');
+	getByText('Du kan bare taste inn tall (0-9)');
 });
